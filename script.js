6d631407f711debae35c0fa09b2596c08c1a200b
padi-dev-tuannh <tuannh@paditech.com>
2026-04-02 15:14:29 +0700

    Update birthday greeting messages in script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ var w = c.width = window.innerWidth,
 		hh = h / 2,
 		
 		opts = {
-			strings: [ 'HAPPY BIRTHDAY', 'ĐỒNG!'],
+			strings: [ 'HAPPY BIRTHDAY!', 'CHÚC ĐẠT TUỔI MỚI RỰC RỠ', '83 86' ],
 			charSize: 30,
 			charSpacing: 35,
 			lineHeight: 40,
